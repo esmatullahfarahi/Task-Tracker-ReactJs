97683ebf4b47354142c09c8f74b8e203241fa0ec
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-10-03 14:29:04 -0400

    Add dynamic form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import Header from "./components/Header"; 
 import Tasks from "./components/Tasks"; 
+import AddTask  from "./components/AddTask"; 
+
 const App=() => {
+    const[showAddTask, setShowAddTask]=useState(false)
     const [tasks, setTasks]= useState([
 
     {
@@ -24,6 +27,14 @@ const App=() => {
     },
 ])
 
+// Add Task
+
+const addTask =(task) =>{
+  const id= Math.floor(Math.random()*1000)+1
+  const newTask={id, ...task}
+  setTasks([...tasks, newTask])
+}
+
  // Delete Task
 
  const deleteTask =(id) => {
@@ -40,7 +51,8 @@ const App=() => {
 
   return (
     <div className="container">
-      <Header  />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}  />
+    {showAddTask &&  <AddTask onAdd={addTask} /> }
     {tasks.length > 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}  />: 'No Tasks'}
     </div>
   )
