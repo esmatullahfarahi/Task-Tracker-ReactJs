439b5b35a9985d4bc6df87ae255aead08040405d
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-10-03 13:28:39 -0400

    true and false condition

--- src/App.js
+++ src/App.js
@@ -30,10 +30,18 @@ const App=() => {
   setTasks(tasks.filter((task)=> task.id !==id))
  } 
 
+ // Toggle Reminder
+ const toggleReminder = (id) =>{
+  setTasks(
+    tasks.map((task)=> 
+    task.id === id ? { ... task, reminder:!task.reminder}: task))
+ }
+
+
   return (
     <div className="container">
       <Header  />
-    {tasks.length > 0 ?  <Tasks tasks={tasks} onDelete={deleteTask}  />: 'No Tasks'}
+    {tasks.length > 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}  />: 'No Tasks'}
     </div>
   )
 }
