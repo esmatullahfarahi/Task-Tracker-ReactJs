a476cc69d2d2a8ce781a27724cdbc1b26eb2cd56
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-10-10 14:27:46 -0400

    Routes

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+    <p>Copyright &Copy; 2023</p>
+    <Link To='/about'>About</Link>
+  </footer>
+  )
+}
+
+export default Footer
